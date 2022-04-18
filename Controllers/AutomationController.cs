fead62cd4533e34ea78585e4cb74bd32b756209e
leopapst@hotmail.com <leopapst@hotmail.com>
2022-04-18 14:05:45 -0300

    Adjusted product object

--- Controllers/AutomationController.cs
+++ Controllers/AutomationController.cs
@@ -18,20 +18,16 @@ namespace ServicesAutomation.Controllers
             JObject blockZero = new JObject();
             //blockZero["Code"] = 0;
             
-            //Campo de opções de serviço
-            JArray serviceField = new JArray();
-            JObject service = new JObject();
-            service["FieldKey"] = "quote_section_2DBC39C3-B23A-4589-8247-CE815AE5B85B";
-            service["Id"] = 4402979;
-            service["Name"] = "ARMAZENAGEM DE CONT\u00caINER FCL";
-            serviceField.Add(service);
-
             //Produtos do bloco
             JArray blockProducts = new JArray();
             JObject product = new JObject();
             product["ProductId"] = 4402979;
             product["Quantity"] = 99;
             product["UnitPrice"] = 55;
+
+            JObject productObj = new JObject();
+            productObj["Id"] = 4402979;
+            productObj["Name"] = "1º Periodo";
             blockProducts.Add(product);
             //blockProducts.Add(serviceField);
 
