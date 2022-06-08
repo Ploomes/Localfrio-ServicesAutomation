a0ed00722356f68304b66d7cbfce040223c60988
leopapst@hotmail.com <leopapst@hotmail.com>
2022-06-08 12:08:04 -0300

    Removido endpoints de teste. Testado localmente endpoint funcional para automação.

--- Controllers/AutomationController.cs
+++ Controllers/AutomationController.cs
@@ -7,65 +7,11 @@ namespace ServicesAutomation.Controllers
     [ApiController]
     public class AutomationController
     {
-        [HttpGet]
-        [Route("Products")]
-        public JObject Products()
-        {
-            JObject sections = new JObject();
-
-            JArray arraySections = new JArray();
-
-            JObject blockZero = new JObject();
-            //blockZero["Code"] = 0;
-
-            //Produtos do bloco
-            JArray blockProducts = new JArray();
-            JObject product = new JObject();
-            product["ProductId"] = 4402979;
-            product["Quantity"] = 99;
-            product["UnitPrice"] = 55;
-
-            JObject productObj = new JObject();
-            productObj["Id"] = 4402979;
-            productObj["Name"] = "1º Periodo";
-            product["Product"] = productObj;
-
-            blockProducts.Add(product);
-            //blockProducts.Add(serviceField);
-
-            blockZero.Add("Products", blockProducts);
-            return blockZero;
-        }
-
-        [HttpGet]
-        [Route("Service")]
-        public JObject Service()
-        {
-            JObject sections = new JObject();
-
-            JArray arraySections = new JArray();
-
-            JObject blockZero = new JObject();
-            //blockZero["Code"] = 0;
-
-            //Campo de opções de serviço
-            JArray serviceField = new JArray();
-            JObject service = new JObject();
-            service["FieldKey"] = "quote_section_2DBC39C3-B23A-4589-8247-CE815AE5B85B";
-            service["Id"] = 4402979;
-            service["Name"] = "ARMAZENAGEM DE CONT\u00caINER FCL";
-            serviceField.Add(service);
-            blockZero.Add("OtherProperties", serviceField);
-
-            arraySections.Add(blockZero);
-            sections["Sections"] = arraySections;
-            return sections;
-        }
-
         [HttpPost]
         [Route("ServiceProducts")]
         public JObject GetServiceProducts(JObject body)
         {
+            PlooLib.InstantiateConnection();
             JObject sections = new JObject();
 
             JArray arraySections = new JArray();
@@ -74,23 +20,30 @@ namespace ServicesAutomation.Controllers
 
             int serviceId = (int)body["ServiceId"];
 
-            string urlRequest = $"Products?$filter=FamilyId+eq+{serviceId}";
+            string urlRequest = $"Products?$filter=(Id+eq+{serviceId})&$expand=OtherProperties($filter=FieldId+eq+10119528)";
+            //product_06495A67-033C-4DFB-9290-AAEC721D7C09
 
             JArray getProducts = RequestHandler.MakeRequest(urlRequest, Method.GET);
 
             //Produtos do bloco
             JArray blockProducts = new JArray();
 
-            foreach (JObject productInArray in getProducts)
+            JArray otherProperties = JArray.Parse(getProducts[0]["OtherProperties"].ToString());
+
+            foreach (JObject productInArray in otherProperties)
             {
+                int productId = (int)productInArray["ProductValueId"];
+                urlRequest = $"Products?$filter=(((Id+eq+{productId})))";
+                JObject productGet = RequestHandler.MakeRequest(urlRequest, Method.GET)[0] as JObject;
+
                 JObject product = new JObject();
-                product["ProductId"] = productInArray["Id"];
+                product["ProductId"] = productGet["Id"];
                 product["Quantity"] = 1;
-                product["UnitPrice"] = productInArray["UnitPrice"];
+                product["UnitPrice"] = productGet["UnitPrice"];
 
                 JObject productObj = new JObject();
-                productObj["Id"] = productInArray["Id"];
-                productObj["Name"] = productInArray["Name"];
+                productObj["Id"] = productGet["Id"];
+                productObj["Name"] = productGet["Name"];
                 product["Product"] = productObj;
 
                 blockProducts.Add(product);
